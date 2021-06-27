70fe6bdf72f54027f6c230af4cc2af6a777fda6d
SuperW <william11616@hotmail.com>
2021-06-27 21:53:24 +0800

    Initial Commit

---
+++ AmplifyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmplifyTest
+//
+//  Created by SuperW on 27/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
