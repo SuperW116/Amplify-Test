dd536c528d091a8d64522f4c6c869059818e1fdd
SuperW <william11616@hotmail.com>
2021-07-03 21:39:56 +0800

    Amplify Add Auth

--- AmplifyTest/AppDelegate.swift
+++ AmplifyTest/AppDelegate.swift
@@ -7,14 +7,18 @@
 
 import UIKit
 import CoreData
+import Amplify
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Amplify.Logging.logLevel = .verbose
+        do {
+            try Amplify.configure()
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
         return true
     }
 
